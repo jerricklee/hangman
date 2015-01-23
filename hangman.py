ca396b8dc9aaa4aa7e611440efd8c6942122648f
jerricklee <jerrick_@naver.com>
2015-01-23 14:07:18 +0900

    test6

--- hangman.py
+++ hangman.py
@@ -123,7 +123,7 @@ def checkWrongAnswer(missedLetters, secretWord):
             
 def main():
     """Main application entry point."""
-    print('H A N G M A N by jerrick!!')
+    print('H A N G M A N by jerrick!!!!!!')
     missedLetters = ''
     correctLetters = ''
     gameSucceeded = False
